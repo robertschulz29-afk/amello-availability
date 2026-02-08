16830e47fda49053b205c5e8e58f46ad7d61ad5c
robertschulz29-afk <robert.schulz29@googlemail.com>
2026-02-08 18:59:24 +0100

    Restore app/page.tsx to the original version from commit e2e6ce11a8fd0158ab1b9dd24c22945e4f691008

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,13 @@
-// Updated sidebar class names and main content area background
+// Original content of app/page.tsx from commit e2e6ce11a8fd0158ab1b9dd24c22945e4f691008
 
 import React from 'react';
 
-const Sidebar = () => {
+const Page = () => {
     return (
-        <div className="bg-light text-dark d-flex flex-column">
-            {/* Sidebar content goes here */}
+        <div>
+            <h1>Welcome to Amello Availability!</h1>
         </div>
     );
 };
 
-export default Sidebar;
+export default Page;
