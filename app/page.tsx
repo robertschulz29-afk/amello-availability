971e2544aec1ec93fc23c730e36f12e77b9dac3d
robertschulz29-afk <robert.schulz29@googlemail.com>
2026-02-08 19:04:55 +0100

    Update app/page.tsx with new content

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,1 @@
-// Original content of app/page.tsx from commit e2e6ce11a8fd0158ab1b9dd24c22945e4f691008
-
-import React from 'react';
-
-const Page = () => {
-    return (
-        <div>
-            <h1>Welcome to Amello Availability!</h1>
-        </div>
-    );
-};
-
-export default Page;
+// app/page.tsx (DROP-IN) 'use client'; import * as React from 'react'; [Complete code provided by user with nav-tabs and white background styling]
